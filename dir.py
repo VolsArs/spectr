b16b9709acc116eac13a0e87b149da294598a6eb
VolsArs <73990575+VolsArs@users.noreply.github.com>
2024-09-14 15:18:34 +0500

    dir

---
+++ dir.py
@@ -0,0 +1,14 @@
+import os
+
+
+def list_files(start_path):
+    for root, dirs, files in os.walk(start_path):
+        level = root.replace(start_path, '').count(os.sep)
+        indent = ' ' * 4 * level
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent+'Имя файла - ', f + ' размер - ' + str(os.path.getsize(root+"\\"+f))+"b"))
+
+
+list_files()
